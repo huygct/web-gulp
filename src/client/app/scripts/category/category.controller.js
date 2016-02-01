a75eed01d43652ba2a6a69f2bad439377d748cb7
Nghi TMA <thuynghi@tma.com.vn>
2016-02-01 16:48:29 +0700

    fix product

--- src/client/app/scripts/category/category.controller.js
+++ src/client/app/scripts/category/category.controller.js
@@ -14,12 +14,50 @@
     /**
      *
      */
-    $scope.clearValue = function() {
-      $scope.myModel = undefined;
-    };
-    $scope.save = function() {
-      alert('Form was valid!');
-    };
+
+    var imagePath = 'img/list/60.jpeg';
+    $scope.phones = [
+      { type: 'Home', number: '(555) 251-1234' },
+      { type: 'Cell', number: '(555) 786-9841' },
+      { type: 'Office', number: '(555) 314-1592' }
+    ];
+    $scope.todos = [
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : imagePath,
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      }
+    ];
 
     /**
      *
---
+++ src/client/app/scripts/product/action/actionProduct.js
@@ -0,0 +1,3 @@
+/**
+ * Created by thuynghi on 01/02/2016.
+ */
